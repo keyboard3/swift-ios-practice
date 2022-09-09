32beef6801e8f82463de8d933595e9b7be5f17c2
甘春雨 <keyboard3@icloud.com>
2022-09-09 17:03:01 +0800

    init commit

---
+++ swift-practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-practice
+//
+//  Created by 甘春雨 on 2022/9/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-practice/swift_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_practiceApp.swift
+//  swift-practice
+//
+//  Created by 甘春雨 on 2022/9/9.
+//
+
+import SwiftUI
+
+@main
+struct swift_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
